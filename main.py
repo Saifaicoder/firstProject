cd4a0b942d79d1b9017b9dcd3037e3578eefa647
saifsama <saifmaaloma@gmail.com>
2025-05-17 13:07:15 +0300

    Add some featurs

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ print("Get test")
 print("Read test")
 # main is av
 print("Say OK")
+print("This line written by saif sama")
+#! ss
--- proj.py
+++ proj.py
@@ -1,1 +1,3 @@
-print("This text wrote by saif sama")
+print("This text wrote by mastee")
+print("This line written by saif sama")
+#! ss
